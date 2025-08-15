cd6952f83254effad58c0ec9467417e155a42b8e
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-08-15 19:15:29 +0200

    style: move import statement to top of file

--- bip-0330/minisketch.py
+++ bip-0330/minisketch.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import random
+
 ######## ENCODING and DECODING ########
 
 FIELD_BITS = 32
@@ -30,8 +32,6 @@ def sketch(shortids, capacity):
     return b''.join(elem.to_bytes(4, 'little') for elem in odd_sums)
 
 ######## DECODING only ########
-
-import random
 
 def inv(x):
     """Compute 1/x in GF(2^FIELD_BITS)"""
