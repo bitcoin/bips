aacb66802228579ecf86bbdba840cf4f06f50254
Jon Atack <jon@atack.com>
2026-07-10 11:35:24 -0600

    script: editorial touchups in link-format-chk.sh

--- scripts/link-format-chk.sh
+++ scripts/link-format-chk.sh
@@ -4,7 +4,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #
-# Check wrong mediawiki and markdown link formats
+# Check mediawiki and markdown link formats
 
 ECODE=0
 MEDIAWIKI_ECODE=0
@@ -12,7 +12,7 @@ while IFS= read -r fname; do
     GRES=$(grep -nE '\]\((https?://|\.\./bip-|/bip-)' "$fname")
     if [ "$GRES" != "" ]; then
         if [ $MEDIAWIKI_ECODE -eq 0 ]; then
-            >&2 echo "Github Mediawiki format writes links as [URL text], not as [text](URL):"
+            >&2 echo "GitHub Mediawiki format writes links as [URL text], not as [text](URL):"
         fi
         MEDIAWIKI_ECODE=1
         ECODE=1
@@ -27,7 +27,7 @@ while IFS= read -r fname; do
     GRES=$(grep -nE '\[[[:space:]]*https?://[^][[:space:]]+[[:space:]]+[^][]*\]|\[\[https?://[^][]*\]\]|\[\[(\.\./|/)?bip-[^][]*\]\]' "$fname")
     if [ "$GRES" != "" ]; then
         if [ $MARKDOWN_ECODE -eq 0 ]; then
-            >&2 echo "Github Markdown format writes links as [text](URL), not as [URL text] or [[URL|text]]:"
+            >&2 echo "GitHub Markdown format writes links as [text](URL), not as [URL text] or [[URL|text]]:"
         fi
         MARKDOWN_ECODE=1
         ECODE=1
